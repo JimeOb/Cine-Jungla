3219060fcc39e3fe71980c8b8e4dbff3e7a44049
Daniel Burgos <dfburgosm@correo.udistrital.edu.co>
2022-03-19 19:16:25 -0500

    Se agrega la estructura inicial de carpetas

---
+++ src/main/java/co/edu/udistrital/App.java
@@ -0,0 +1,7 @@
+package co.edu.udistrital;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/co/edu/udistrital/AppTest.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
