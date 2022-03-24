8a0131f41da6e044048ff3f1ac3137011c9f4063
Natharg <nathurg3@gmail.com>
2022-03-23 23:04:47 -0500

    Implementacion arquitectura

--- src/main/java/co/edu/udistrital/App.java
+++
@@ -1,7 +0,0 @@
-package co.edu.udistrital;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ src/main/java/co/edu/udistrital/cine/Launcher.java
@@ -0,0 +1,28 @@
+package co.edu.udistrital.cine;
+
+import co.edu.udistrital.cine.presentacion.Modelo;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Launcher{
+
+    private Modelo miApp;
+
+    public Launcher(){
+        miApp = new Modelo();
+        miApp.iniciar();
+    }
+        
+    public static void main(String[] args){
+        new Launcher();
+    }
+    
+}
