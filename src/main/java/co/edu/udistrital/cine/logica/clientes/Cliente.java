682bb66385fe605b9cbe6f6f76776656b7cf1227
Daniel Burgos <dfburgosm@correo.udistrital.edu.co>
2022-04-02 17:46:14 -0500

    Se agrega hibernate para agilizar el proceso de desarrollo. Tambien se agrega la entidad cliente

---
+++ src/main/java/co/edu/udistrital/cine/logica/clientes/Cliente.java
@@ -0,0 +1,75 @@
+package co.edu.udistrital.cine.logica.clientes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    
+    @Id    
+    @Column(name = "id_cliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column
+    private String nombre;
+    
+    @Column
+    private String email;
+    
+    @Column
+    private String password;
+    
+    @Column
+    private Integer puntos;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getPuntos() {
+        return puntos;
+    }    
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPuntos(Integer puntos) {
+        this.puntos = puntos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", email=" + email + ", password=" + password + ", puntos=" + puntos + '}';
+    }
+}
---
+++ src/test/java/co/edu/udistrital/hibernate/HibernateTest.java
@@ -0,0 +1,100 @@
+package co.edu.udistrital.hibernate;
+
+import co.edu.udistrital.cine.logica.clientes.Cliente;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HibernateTest {
+
+    private static SessionFactory sessionFactory;
+    private Cliente cliente;
+
+    @BeforeAll
+    public static void setupAll() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .loadProperties("hibernate.properties")
+                .build();
+
+        Metadata metadata = new MetadataSources(registry)
+                .addAnnotatedClass(Cliente.class)
+                .buildMetadata();
+
+        sessionFactory = metadata.buildSessionFactory();
+
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        sessionFactory.close();
+    }
+
+    @BeforeEach
+    public void setup() {
+        cliente = new Cliente();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.persist(cliente);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Test
+    public void hibernateShouldContainsAClient() {
+
+        Session session = sessionFactory.openSession();
+        List<Cliente> clientes = getClientesInDB(session);
+        session.close();
+
+        System.out.println(clientes.get(0));
+
+        Assertions.assertEquals(1, clientes.size());
+        Assertions.assertEquals(cliente, clientes.get(0));
+
+    }
+
+    @Test
+    public void hibernateShouldDeleteAClient() {
+        Session session = sessionFactory.openSession();
+        session.remove(cliente);
+
+        final List<Cliente> clientes = getClientesInDB(session);
+        session.close();
+
+        Assertions.assertEquals(0, clientes.size());
+    }
+
+    @Test
+    public void hibernateShouldUpdateAnOldRecord() {
+        Session session = sessionFactory.openSession();
+
+        cliente.setId(1);
+        cliente.setNombre("Prueba");
+        cliente.setEmail("prueba@correo");
+        cliente.setPuntos(1);
+
+        session.beginTransaction();
+        session.merge(cliente);
+        session.getTransaction().commit();
+
+        final Cliente updatedClient = session.byId(Cliente.class).load(1);
+        System.out.println(updatedClient);
+        
+        Assertions.assertEquals(cliente.getEmail(), updatedClient.getEmail());
+
+    }
+
+    public List<Cliente> getClientesInDB(Session session) {
+        return session.createQuery("FROM Cliente", Cliente.class).getResultList();
+    }
+}
