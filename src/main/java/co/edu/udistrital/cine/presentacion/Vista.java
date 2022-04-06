3a7a12940426d5497cb088abe582f27f1451cc25
SantiagoGarcia8 <sjgarciat@correo.udistrital.edu.co>
2022-04-06 08:20:37 -0500

    ajustes

--- src/main/java/co/edu/udistrital/cine/presentacion/Vista.java
+++ src/main/java/co/edu/udistrital/cine/presentacion/Vista.java
@@ -38,6 +38,7 @@ public class Vista extends javax.swing.JFrame {
         jLabel12 = new javax.swing.JLabel();
         jtxContrasena2 = new javax.swing.JPasswordField();
         btnIniciar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -54,7 +55,7 @@ public class Vista extends javax.swing.JFrame {
         pnInfo.add(jtxUsuario1, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 280, 290, 40));
 
         jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel11.setText("IMAGEN");
+        jLabel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Assets/CINE.jpg"))); // NOI18N
         jLabel11.setToolTipText("");
         pnInfo.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 60, 530, 200));
 
@@ -69,7 +70,12 @@ public class Vista extends javax.swing.JFrame {
         btnIniciar.setBackground(new java.awt.Color(238, 28, 28));
         btnIniciar.setFont(new java.awt.Font("Bell MT", 0, 18)); // NOI18N
         btnIniciar.setText("Iniciar Sesión");
-        pnInfo.add(btnIniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 480, 180, 40));
+        pnInfo.add(btnIniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 470, 180, 40));
+
+        jButton1.setBackground(new java.awt.Color(102, 153, 255));
+        jButton1.setFont(new java.awt.Font("Bell MT", 0, 18)); // NOI18N
+        jButton1.setText("Registrar Usuario");
+        pnInfo.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 480, 180, 40));
 
         getContentPane().add(pnInfo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 810, 650));
 
@@ -79,6 +85,7 @@ public class Vista extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnIniciar;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel5;
